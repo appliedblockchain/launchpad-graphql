f2e2fb8551f1a2a28ef02270aec36e22e5da4fca
makevoid <makevoid@gmail.com>
2019-03-30 08:23:34 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import gql from 'graphql-tag'
+import { Query, Mutation } from 'react-apollo'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+import './App.css'
+
+const GET_POSTS = gql`
+{
+  posts {
+    id
+    title
+    description
+    likes
+    comments {
+      text
+      id
+    }
   }
 }
+`
 
-export default App;
+const LIKE_POST = gql`
+
+  mutation($id: ID!) {
+    update_posts(input: { starrableId: $id }, _inc: {likes: 1})
+  }
+
+`;
+
+const App = () => (
+  <Query query={GET_POSTS}>
+    {({ data: { posts }, loading }) => {
+      if (loading || !posts) {
+        return (<div>Loading ...</div>)
+      }
+      return (
+        <div>
+          <PostList posts={posts} />
+        </div>
+      )
+    }}
+  </Query>
+)
+
+const PostList = ({ posts }) => (
+  <div className="posts">
+    {posts && posts.map((post) => (
+      <div key={post.id} className="post">
+        {post.title}
+        <CommentsList comments={post.comments} />
+        <a className="likeBtn">👍</a>
+        <span className="likes">({post.likes})</span>
+      </div>
+    ))}
+  </div>
+)
+
+const CommentsList = ({ comments }) => (
+  <ul>
+    {comments && comments.map((comment) => (
+      <li key={comment.id} className="comment">
+        {comment.text}
+      </li>
+    ))}
+  </ul>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const cache = new InMemoryCache()
+
+const GRAPHSQL_API_ROOT = 'http://134.209.89.56/v1alpha1/graphql'
+
+const httpLink = new HttpLink({
+  uri: GRAPHSQL_API_ROOT,
+  headers: {
+    // auth goes here
+  },
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+)
