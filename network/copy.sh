b9b7ebe9f7ac3c6e9401ac4f26442983a13ad71a
Alaan Rahim <alaan@appliedblockchain.com>
2019-04-25 10:34:39 +0100

    use rsync in favour of scp

--- network/copy.sh
+++ network/copy.sh
@@ -4,5 +4,5 @@ USER=ec2-user
 IP=54.77.246.109
 
 scp docker-compose.yml $USER@$IP:/home/ec2-user/docker-compose.yml
-scp -r auth-server $USER@$IP:/home/ec2-user
-scp -r app $USER@$IP:/home/ec2-user
+rsync -e ssh -av --exclude='node_modules' auth-server $USER@$IP:/home/ec2-user
+rsync -e ssh -av --exclude='node_modules' app $USER@$IP:/home/ec2-user
